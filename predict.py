daa4a63c540000d36827067f2aca3d682b115751
MaddRaven <faivrefx@gmail.com>
2024-10-03 16:19:57 +0200

    graph

--- predict.py
+++ predict.py
@@ -1,12 +1,17 @@
 def	predict(mileage, theta0, theta1):
 	return theta0 + (theta1 * mileage)
 
-theta0 = 0
-theta1 = 0
-mileage = input("Enter mileage: ")
-try:
-	mileage = float(mileage)
-	predict = predict(mileage, theta0, theta1)
-	print(f"Price estimated: {predict:.2f}")
-except ValueError:
-	print("Incorrect input")
+
+def main():
+	theta0 = 0
+	theta1 = 0
+	mileage = input("Enter mileage: ")
+	try:
+		mileage = float(mileage)
+		predict = predict(mileage, theta0, theta1)
+		print(f"Price estimated: {predict:.2f}")
+	except ValueError:
+		print("Incorrect input")
+
+if __name__ == "__main__":
+    main()
