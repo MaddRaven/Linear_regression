92956d076881dd77712e7742c247077f48829552
MaddRaven <faivrefx@gmail.com>
2024-10-06 18:36:57 +0200

    cas d'erreurs

--- predict.py
+++ predict.py
@@ -8,35 +8,41 @@ def	predict(mileage, theta0, theta1, norm_mean, norm_std):
 
 def main():
 	while True:
-		mileage = input("Enter mileage: ")
-
 		try:
-			mileage = float(mileage)
-		except ValueError:
-			print("Incorrect input")
-			exit(1)
+			mileage = input("Enter mileage: ")
 
-		file = 'trained_params.txt'
-
-		if os.path.isfile(file) == False:
-			theta0 = 0
-			theta1 = 0
-			norm_mean = 0
-			norm_std = 1
-		else:
 			try:
-				theta0, theta1, norm_mean, norm_std = np.loadtxt('trained_params.txt')
-			except Exception as e:
-				print("Error extracting datas")
+				mileage = float(mileage)
+			except ValueError:
+				print("Incorrect input")
 				exit(1)
 
-		prediction = predict(mileage, theta0, theta1, norm_mean, norm_std)
-		if prediction < 0:
-			print("Mileage too high")
-		elif mileage < 0:
-			print("Mileage should be positive")
-		else:
-			print(f"Price estimated: {prediction:.2f}")
+			file = 'trained_params.txt'
+
+			if os.path.isfile(file) == False:
+				theta0 = 0
+				theta1 = 0
+				norm_mean = 0
+				norm_std = 1
+			else:
+				try:
+					theta0, theta1, norm_mean, norm_std = np.loadtxt('trained_params.txt')
+				except Exception as e:
+					print("Error extracting datas")
+					exit(1)
+
+			prediction = predict(mileage, theta0, theta1, norm_mean, norm_std)
+			if prediction < 0:
+				print("Mileage too high")
+			elif mileage < 0:
+				print("Mileage should be positive")
+			else:
+				print(f"Price estimated: {prediction:.2f}")
+
+		except KeyboardInterrupt:
+			print("\nProgram interrupted")
+			break
+
 
 if __name__ == "__main__":
     main()
