f56eefdd235d6178116bb5dca1966fd59af3823b
MaddRaven <faivrefx@gmail.com>
2024-10-07 12:50:43 +0200

    precision

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 from print import print_graph
+from predict import predict
 
 def train_model(x, y, x_mean, x_std):
 	learning_rate = 0.01
@@ -20,6 +21,28 @@ def train_model(x, y, x_mean, x_std):
 	return theta0, theta1
 
 
+def calculate_mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def calculate_rmse(y_true, y_pred):
+    return np.sqrt(np.mean((y_true - y_pred)**2))
+
+
+def r2_score(y_true, y_pred):
+    ss_res = np.sum((y_true - y_pred)**2)
+    ss_tot = np.sum((y_true - np.mean(y_true))**2)
+    
+    if abs(ss_tot) < 1e-10:
+        return 0
+    
+    return 1 - (ss_res / ss_tot)
+
+
+def calculate_mse(y_true, y_pred):
+    return np.mean((y_true - y_pred)**2)
+
+
 def	main():
 	try:
 		data = np.genfromtxt('data.csv', delimiter=',', skip_header=1)
@@ -35,6 +58,21 @@ def	main():
 	theta0, theta1 = train_model(x, y, x_mean, x_std)
 
 	np.savetxt('trained_params.txt', [[theta0], [theta1], x_mean, x_std])
+
+	y = data[:, 1]
+	y_pred = []
+	for _ in range(len(x)):
+		y_pred.append(predict(x[_], theta0, theta1, x_mean, x_std)[0])
+
+	mae = calculate_mae(np.sum(y), np.sum(y_pred))
+	rmse = calculate_rmse(np.sum(y), np.sum(y_pred))
+	r2 = r2_score(np.sum(y), np.sum(y_pred))
+	mse = calculate_mse(np.sum(y), np.sum(y_pred))
+
+	print(f"MAE: {mae}")
+	print(f"RMSE: {rmse}")
+	print(f"R-Squared: {r2}")
+	print(f"MSE: {mse}")
 
 	m = len(x)
 	theta = [theta0, theta1]
